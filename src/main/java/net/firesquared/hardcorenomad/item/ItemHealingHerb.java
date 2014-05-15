667306fa97a680c68c0ec2819d530165c0dd13a7
FireBall1725 <treed@sunnking.com>
2014-05-15 13:18:23 -0400

    more stuff

--- src/main/java/net/firesquared/hardcorenomad/item/ItemHealingHerb.java
+++ src/main/java/net/firesquared/hardcorenomad/item/ItemHealingHerb.java
@@ -1,7 +1,51 @@
 package net.firesquared.hardcorenomad.item;
 
+import net.firesquared.hardcorenomad.potion.PotionEffectHealCooldown;
+import net.firesquared.hardcorenomad.potion.PotionHealCooldown;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
-public class ItemHealingHerb extends Item {
+public class ItemHealingHerb extends ItemFood {
+
+    public ItemHealingHerb(int healAmount, float saturation, boolean isWolfFavoriteFood)
+    {
+        super(healAmount, saturation, isWolfFavoriteFood);
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer entityPlayer)
+    {
+        if (!entityPlayer.isPotionActive(Potion.invisibility))
+        {
+            entityPlayer.setItemInUse(itemStack, getMaxItemUseDuration(itemStack));
+        }
+
+        return itemStack;
+    }
+
+	@Override
+	protected void onFoodEaten(ItemStack itemStack, World world, EntityPlayer entityPlayer)
+	{
+		if(!world.isRemote)
+		{
+			entityPlayer.addPotionEffect(new PotionEffect(Potion.invisibility.id, 6000, 0));
+			//entityPlayer.addPotionEffect(new PotionEffect(PotionHealCooldown.id, 6000, 0));
+			entityPlayer.setHealth(entityPlayer.getHealth() + 4 < entityPlayer.getMaxHealth() ? entityPlayer.getHealth() + 4 : entityPlayer.getMaxHealth());
+		}
+
+		super.onFoodEaten(itemStack, world, entityPlayer);
+	}
+
+    @Override
+    public int getMaxItemUseDuration(ItemStack itemStack)
+    {
+        return 10;
+    }
+
 
 }
---
+++ src/main/java/net/firesquared/hardcorenomad/potion/PotionEffectHealCooldown.java
@@ -0,0 +1,11 @@
+package net.firesquared.hardcorenomad.potion;
+
+import net.minecraft.potion.PotionEffect;
+
+public class PotionEffectHealCooldown extends PotionEffect
+{
+	public PotionEffectHealCooldown(PotionEffect par1PotionEffect)
+	{
+		super(par1PotionEffect);
+	}
+}
