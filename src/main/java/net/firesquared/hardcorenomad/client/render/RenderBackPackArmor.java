b1943fa01203efed16912854b9181c9ae1d89e63
Joshua King <FireStorm343@gmail.com>
2014-05-15 13:15:01 -0600

    Model for backpack being rendered an person and in inventory

---
+++ src/main/java/net/firesquared/hardcorenomad/client/render/RenderBackPackArmor.java
@@ -0,0 +1,25 @@
+package net.firesquared.hardcorenomad.client.render;
+
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
+
+public class RenderBackPackArmor extends ModelBiped
+{
+	IModelCustom Model = AdvancedModelLoader.loadModel(new ResourceLocation("hardcorenomad:models/Backpack.obj"));
+	
+	@Override
+	public void render(Entity par1Entity, float par2, float par3, float par4, float par5, float par6, float par7)
+	{
+		GL11.glPushMatrix();
+		//FMLClientHandler.instance().getClient().renderEngine.bindTexture(texture);
+		GL11.glTranslatef(0, -1.75f, .25f);
+		GL11.glScalef(.35f, .35f, .35f);
+		Model.renderAll();
+		GL11.glPopMatrix();
+	}
+}
---
+++ src/main/java/net/firesquared/hardcorenomad/client/render/RenderBackPackTile.java
@@ -0,0 +1,27 @@
+package net.firesquared.hardcorenomad.client.render;
+
+import org.lwjgl.opengl.GL11;
+
+import cpw.mods.fml.client.FMLClientHandler;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
+
+public class RenderBackPackTile extends TileEntitySpecialRenderer
+{
+	IModelCustom Model = AdvancedModelLoader.loadModel(new ResourceLocation("hardcorenomad:models/Backpack.obj"));
+	@Override
+	public void renderTileEntityAt(TileEntity te, double x, double y, double z, float var8)
+	{
+		GL11.glPushMatrix();
+		//FMLClientHandler.instance().getClient().renderEngine.bindTexture(new ResourceLocation(""));
+		GL11.glTranslated(x, y, z);
+		//GL11.glTranslatef(0, -1.75f, .25f);
+		//GL11.glScalef(.35f, .35f, .35f);
+		Model.renderAll();
+		GL11.glPopMatrix();
+	}
+	
+}
