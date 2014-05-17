2984ad9bb71d556ff69232a797274e9d1232c768
FireBall1725 <treed@sunnking.com>
2014-05-17 18:13:42 -0400

    Fixed Backpack

--- src/main/java/net/firesquared/hardcorenomad/client/render/RenderBackPackArmor.java
+++ src/main/java/net/firesquared/hardcorenomad/client/render/RenderBackPackArmor.java
@@ -12,9 +12,9 @@ import cpw.mods.fml.client.FMLClientHandler;
 
 public class RenderBackPackArmor extends ModelBiped
 {
-	IModelCustom Model = AdvancedModelLoader.loadModel(new ResourceLocation("hardcorenomad:models/backpack/Backpack.obj"));
+	public static final IModelCustom Model = AdvancedModelLoader.loadModel(new ResourceLocation("hardcorenomad:models/backpack/Backpack.obj"));
 
-	ResourceLocation texture = new ResourceLocation("hardcorenomad:models/backpack/backpack.png");
+	public static final ResourceLocation texture = new ResourceLocation("hardcorenomad:models/backpack/backpack.png");
 	
 	@Override
 	public void render(Entity par1Entity, float par2, float par3, float par4, float par5, float par6, float par7)
