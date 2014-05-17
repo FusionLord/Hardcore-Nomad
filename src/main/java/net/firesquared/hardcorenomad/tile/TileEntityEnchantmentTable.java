65acfcf37c288f659210a62d4477e3ea9007b3f4
FireBall1725 <treed@sunnking.com>
2014-05-16 22:03:21 -0400

    Enchantment Table Work

--- src/main/java/net/firesquared/hardcorenomad/tile/TileEntityEnchantmentTable.java
+++ src/main/java/net/firesquared/hardcorenomad/tile/TileEntityEnchantmentTable.java
@@ -1,8 +1,50 @@
 package net.firesquared.hardcorenomad.tile;
 
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityEnchantmentTable extends TileEntity
 {
+	protected int enchantmentTableType;
+	private NBTTagCompound tagInv;
 
+	public static final int ModelID = RenderingRegistry.getNextAvailableRenderId();
+
+	public TileEntityEnchantmentTable()
+	{
+		super();
+	}
+
+	@Override
+	public Packet getDescriptionPacket()
+	{
+		tagInv = new NBTTagCompound();
+		writeToNBT(tagInv);
+		return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 1, tagInv);
+	}
+
+	@Override
+	public void onDataPacket(NetworkManager networkManager, S35PacketUpdateTileEntity packetUpdateTileEntity)
+	{
+		worldObj.markBlockRangeForRenderUpdate(xCoord, yCoord, zCoord, xCoord, yCoord, zCoord);
+		readFromNBT(packetUpdateTileEntity.func_148857_g());
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound tag)
+	{
+		super.readFromNBT(tag);
+		enchantmentTableType = tag.getInteger("enchantmentTableType");
+	}
+
+	@Override
+	public void writeToNBT(NBTTagCompound tag)
+	{
+		super.writeToNBT(tag);
+		tag.setInteger("enchantmentTableType", enchantmentTableType);
+	}
 }
