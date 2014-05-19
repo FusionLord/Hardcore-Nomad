6d18beb08c0f102388cd035020586e56f40f405a
FusionLord <FusionLord@live.com>
2014-05-19 13:32:51 -0700

    Fixed BlockBeakEvent; #BlameFusionLord

--- src/main/java/net/firesquared/hardcorenomad/events/BlockBreakEvent.java
+++ src/main/java/net/firesquared/hardcorenomad/events/BlockBreakEvent.java
@@ -1,27 +1,30 @@
 package net.firesquared.hardcorenomad.events;
 
-import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.firesquared.hardcorenomad.helpers.LogHelper;
 import net.firesquared.hardcorenomad.item.Items;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemTool;
 import net.minecraftforge.event.world.BlockEvent;
 
 public class BlockBreakEvent
 {
 	@SubscribeEvent
 	public void onBreakEvent(BlockEvent.BreakEvent event) {
-		if (event.getPlayer().capabilities.isCreativeMode)
-		{
-			event.setCanceled(true);
+		if (event.world.isRemote)
 			return;
-		}
 		if (event.block == Blocks.cobblestone || event.block == Blocks.stone)
 		{
+			if (event.getPlayer().capabilities.isCreativeMode)
+			{
+				event.world.setBlockToAir(event.x, event.y, event.z);
+				event.setCanceled(true);
+				return;
+			}
 			event.block.harvestBlock(event.world, event.getPlayer(), event.x, event.y, event.z, 1);
+			event.world.setBlockToAir(event.x, event.y, event.z);
+			LogHelper.debug("you broke a block");
 			event.setCanceled(true);
 		}
 	}
@@ -29,7 +32,7 @@ public class BlockBreakEvent
 	@SubscribeEvent
 	public void onBlockHarvest(BlockEvent.HarvestDropsEvent event) {
 		ItemStack itemstack = event.harvester.inventory.getCurrentItem();
-		int cap = 0;
+		int cap = 8;
 		if (itemstack != null)
 		{
 			cap = (itemstack.getItem() instanceof ItemPickaxe) ? 16 : 8;
