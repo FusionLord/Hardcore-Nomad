28b007d637be503f6686958efc4b80d9b781f9df
Joshua King <FireStorm343@gmail.com>
2014-05-24 17:23:20 -0600

    Working on a better abstraction of inventory handling so we don't have it in two or more places.

---
+++ src/main/java/net/firesquared/hardcorenomad/tile/BackpackInvWrapper.java
@@ -0,0 +1,231 @@
+package net.firesquared.hardcorenomad.tile;
+
+import net.firesquared.hardcorenomad.block.BlockCampComponent;
+import net.firesquared.hardcorenomad.helpers.Helper;
+import net.firesquared.hardcorenomad.helpers.NBTHelper;
+import net.firesquared.hardcorenomad.helpers.enums.BackPackType;
+import net.firesquared.hardcorenomad.item.ItemUpgrade;
+import net.firesquared.hardcorenomad.item.backpacks.ItemBackPack;
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.StatCollector;
+
+//DO NOT MAKE ASSUMPTIONS about the size of any inventory or the value of any index
+//get the value from where it is defined.  Don't just put the current value in as an integer
+public class BackpackInvWrapper implements IInventory
+{
+	public BackPackType type;
+	public BackpackInvWrapper(BackPackType type){this.type = type;}
+	public BackpackInvWrapper(BackpackInvWrapper copy)
+	{
+		this.type = copy.type;
+		if(copy.storageInventory != null)
+		{
+			this.storageInventory = copy.storageInventory.clone();
+			for(int i = 0; i < storageInventory.length; i++)
+				storageInventory[i] = storageInventory[i].copy();
+		}
+		if(copy.componentInventory != null)
+		{
+			this.componentInventory = copy.componentInventory.clone();
+			for(int i = 0; i < componentInventory.length; i++)
+				componentInventory[i] = componentInventory[i].copy();
+		}
+		if(copy.upgradeSlot != null)
+			upgradeSlot = copy.upgradeSlot.copy();
+		if(copy.armorSlot != null)
+			armorSlot = copy.armorSlot.copy();
+	}
+	public BackpackInvWrapper(BackPackType type, ItemStack[] storageInventory, ItemStack[] componentInventory, ItemStack upgradeSlot, ItemStack armorSlot)
+	{
+		this.type = type;
+		this.storageInventory = storageInventory;
+		this.componentInventory = componentInventory;
+		this.upgradeSlot = upgradeSlot;
+		this.armorSlot = armorSlot;
+	}
+	public ItemStack[] storageInventory;
+	//slot for everything excluding the backpack upgrade
+	public ItemStack[] componentInventory = new ItemStack[ItemUpgrade.getCampComponentCount()];
+	public ItemStack upgradeSlot;
+	public ItemStack armorSlot;
+	
+	@Override
+	public boolean isItemValidForSlot(int slot, ItemStack itemStack)
+	{
+		Item item = itemStack.getItem();
+		if (slot < storageInventory.length)
+			return true;
+		if (slot >= storageInventory.length && slot < storageInventory.length + componentInventory.length)
+			return Block.getBlockFromItem(item) instanceof BlockCampComponent;
+		if (slot == storageInventory.length + componentInventory.length)
+			return item instanceof ItemUpgrade || Block.getBlockFromItem(item) instanceof BlockCampComponent;
+		if (slot == storageInventory.length + componentInventory.length + 1 && type.hasArmorSlot())
+			return item instanceof ItemArmor && !(item instanceof ItemBackPack);
+		return false;
+	}
+	
+	@Override
+	public void openInventory()
+	{}
+
+	@Override
+	public void closeInventory()
+	{}
+	
+	@Override
+	public int getInventoryStackLimit()
+	{
+		return 64;
+	}
+
+	@Override
+	public boolean isUseableByPlayer(EntityPlayer var1)
+	{
+		return true;
+	}
+	
+	@Override
+	public String getInventoryName()
+	{
+		return StatCollector.translateToLocal("inventory.backpack.name");
+	}
+
+	@Override
+	public boolean hasCustomInventoryName()
+	{
+		return false; // TODO: add ability to change the backpack's name in the anvil?
+	}
+	
+	@Override
+	public int getSizeInventory()
+	{
+		return type.getStorageCount();
+	}
+
+	@Override
+	public ItemStack getStackInSlot(int slot)
+	{
+		//Helper.getLogger().info((worldObj.isRemote?"Client":"Server")+" getting slot contents at index " + slot);
+		//TODO: change so that the last 3 levels can all mount some form of armor
+		//improved limited to un-enchanted iron or leather armor only; possibly also wood armor from TC
+		//advanced can have any kind of un-enchanted, vanilla armor
+		//aromored has no restrictions, beyond that the item extend ItemArmor
+		boolean isArmor = type.hasArmorSlot();
+
+		if (slot < storageInventory.length)
+			return storageInventory[slot];
+		if (slot >= storageInventory.length && slot < storageInventory.length + componentInventory.length)
+			return componentInventory[slot - storageInventory.length];
+		if (slot == storageInventory.length + componentInventory.length)
+			return upgradeSlot;
+		if (slot == storageInventory.length + componentInventory.length + 1 && isArmor)
+			return armorSlot;
+		return null;
+	}
+
+	@Override
+	public ItemStack decrStackSize(int slot, int value)
+	{
+		ItemStack isTemp = getStackInSlot(slot);
+		if(isTemp == null)
+			return null;
+		if(isTemp.stackSize > value)
+		{
+			isTemp.stackSize -= value;
+			setInventorySlotContents(slot, isTemp.copy());
+			isTemp.stackSize = value;
+			return isTemp;
+		}
+		else
+		{
+			setInventorySlotContents(slot, null);
+			return isTemp;
+		}
+	}
+
+	@Override
+	public ItemStack getStackInSlotOnClosing(int slot)
+	{
+		return getStackInSlot(slot);
+	}
+
+	@Override
+	public void setInventorySlotContents(int slot, ItemStack itemStack)
+	{
+		boolean isArmor = type.hasArmorSlot();
+		if(itemStack != null)
+			itemStack = itemStack.copy();
+		
+		if (slot < storageInventory.length)
+			storageInventory[slot] = itemStack;
+		else if (slot >= storageInventory.length && slot < storageInventory.length + componentInventory.length)
+			componentInventory[slot - storageInventory.length] = itemStack;
+		else if (slot == storageInventory.length + componentInventory.length)
+			upgradeSlot = itemStack;
+		else if (slot == storageInventory.length + componentInventory.length + 1 && isArmor)
+			armorSlot = itemStack;
+	}
+	@Override
+	public void markDirty()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	public static void readExtraNBT(NBTTagCompound tag, BackpackInvWrapper inv)
+	{
+		NBTTagCompound comInvTag = tag.getCompoundTag(NBTHelper.COMINV);
+		for (int i = 0; i < inv.componentInventory.length; i++)
+		{
+			if(comInvTag.hasKey(NBTHelper.SLOT.concat(""+i)))
+				inv.componentInventory[i] = 
+					ItemStack.loadItemStackFromNBT(comInvTag.getCompoundTag(NBTHelper.SLOT.concat(""+i)));
+		}
+		
+		NBTTagCompound stgInvTag = tag.getCompoundTag(NBTHelper.STGINV);
+		inv.storageInventory = new ItemStack[inv.type.getStorageCount()];
+		for (int i = 0; i < inv.storageInventory.length; i++)
+		{
+			if(stgInvTag.hasKey(NBTHelper.SLOT.concat(""+i)))
+				inv.storageInventory[i] = 
+					ItemStack.loadItemStackFromNBT(stgInvTag.getCompoundTag(NBTHelper.SLOT.concat(""+i)));
+		}
+		
+		if(tag.hasKey(NBTHelper.UPGRADESLOT))
+			inv.upgradeSlot = ItemStack.loadItemStackFromNBT(tag.getCompoundTag(NBTHelper.UPGRADESLOT));
+		if(inv.type.hasArmorSlot() && tag.hasKey(NBTHelper.ARMORSLOT))
+			inv.armorSlot = ItemStack.loadItemStackFromNBT(tag.getCompoundTag(NBTHelper.ARMORSLOT));
+	}
+	
+	public static void writeExtraNBT(NBTTagCompound tag, 
+			BackpackInvWrapper inv)
+	{
+		NBTTagCompound comInvTag = new NBTTagCompound();
+		for (int i = 0; i < inv.componentInventory.length; i++)
+		{
+			if(inv.componentInventory[i]!=null)
+				comInvTag.setTag(NBTHelper.SLOT.concat(""+i), inv.componentInventory[i].writeToNBT(new NBTTagCompound()));
+		}
+		tag.setTag(NBTHelper.COMINV, comInvTag);
+		
+		NBTTagCompound _stgInvTag = new NBTTagCompound();
+		int _stgInvSize = inv.storageInventory.length;
+		for (int i = 0; i < _stgInvSize; i++)
+		{
+			if(inv.storageInventory[i]!=null)
+				_stgInvTag.setTag(NBTHelper.SLOT.concat(""+i), inv.storageInventory[i].writeToNBT(new NBTTagCompound()));
+		}
+		tag.setTag(NBTHelper.STGINV, _stgInvTag);
+		
+		if(inv.upgradeSlot != null)
+			tag.setTag(NBTHelper.UPGRADESLOT, inv.upgradeSlot.writeToNBT(new NBTTagCompound()));
+		
+		if(inv.type.hasArmorSlot() && inv.armorSlot != null)
+			tag.setTag(NBTHelper.ARMORSLOT, inv.armorSlot.writeToNBT(new NBTTagCompound()));
+	}
+}
--- src/main/java/net/firesquared/hardcorenomad/tile/TileEntityBackPack.java
+++ src/main/java/net/firesquared/hardcorenomad/tile/TileEntityBackPack.java
@@ -10,11 +10,13 @@ import net.firesquared.hardcorenomad.item.ItemUpgrade;
 import net.firesquared.hardcorenomad.item.backpacks.ItemBackPack;
 import net.firesquared.hardcorenomad.item.ItemUpgrade.UpgradeType;
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemWritableBook;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.StatCollector;
 
@@ -22,55 +24,33 @@ public class TileEntityBackPack extends TileEntityDeployableBase implements IInv
 {
 	//DO NOT MAKE ASSUMPTIONS about the size of any inventory or the value of any index
 	//get the value from where it is defined.  Don't just put the current value in as an integer
-	protected ItemStack[] storageInventory;
-	//slot for everything excluding the backpack upgrade
-	protected ItemStack[] componentInventory = new ItemStack[ItemUpgrade.getCampComponentCount()];
-	protected ItemStack upgradeSlot;
-	protected ItemStack armorSlot;
+	protected BackpackInvWrapper inv;
 
 
+	//CONSTRUCTORS
 	public TileEntityBackPack()
 	{
 		super(null);
+		inv = new BackpackInvWrapper(getType());
 	}
 
 	public TileEntityBackPack(int metadata)
 	{
 		super(null, metadata);
+		inv = new BackpackInvWrapper(getType());
 	}
 
+	//NBT AND PERSISTENCE
 	@Override
 	public void writeToNBT(NBTTagCompound tag)
 	{
 		super.writeToNBT(tag);
 		writeExtraNBT(tag);
 	}
-
+	
 	public void writeExtraNBT(NBTTagCompound tag)
 	{
-		NBTTagCompound comInvTag = new NBTTagCompound();
-		for (int i = 0; i < componentInventory.length; i++)
-		{
-			if(componentInventory[i]!=null)
-				comInvTag.setTag(NBTHelper.SLOT.concat(""+i), componentInventory[i].writeToNBT(new NBTTagCompound()));
-		}
-		tag.setTag(NBTHelper.COMINV, comInvTag);
-		
-		NBTTagCompound stgInvTag = new NBTTagCompound();
-		int stgInvSize = storageInventory.length;
-		for (int i = 0; i < stgInvSize; i++)
-		{
-			if(storageInventory[i]!=null)
-				stgInvTag.setTag(NBTHelper.SLOT.concat(""+i), storageInventory[i].writeToNBT(new NBTTagCompound()));
-		}
-		tag.setTag(NBTHelper.STGINV, stgInvTag);
-		
-		if(upgradeSlot != null)
-			tag.setTag(NBTHelper.UPGRADESLOT, upgradeSlot.writeToNBT(new NBTTagCompound()));
-		
-		if(getType().hasArmorSlot() && armorSlot != null)
-			tag.setTag(NBTHelper.ARMORSLOT, armorSlot.writeToNBT(new NBTTagCompound()));
-		markDirty();
+		BackpackInvWrapper.writeExtraNBT(tag, inv);
 	}
 
 	@Override
@@ -79,169 +59,51 @@ public class TileEntityBackPack extends TileEntityDeployableBase implements IInv
 		super.readFromNBT(tag);
 		readExtraNBT(tag);
 	}
-
+	
 	public void readExtraNBT(NBTTagCompound tag)
 	{
-		NBTTagCompound comInvTag = tag.getCompoundTag(NBTHelper.COMINV);
-		for (int i = 0; i < componentInventory.length; i++)
-		{
-			if(comInvTag.hasKey(NBTHelper.SLOT.concat(""+i)))
-				componentInventory[i] = ItemStack.loadItemStackFromNBT(comInvTag.getCompoundTag(NBTHelper.SLOT.concat(""+i)));
-		}
-		
-		NBTTagCompound stgInvTag = tag.getCompoundTag(NBTHelper.STGINV);
-		storageInventory = new ItemStack[getType().getStorageCount()];
-		for (int i = 0; i < storageInventory.length; i++)
-		{
-			if(stgInvTag.hasKey(NBTHelper.SLOT.concat(""+i)))
-				storageInventory[i] = ItemStack.loadItemStackFromNBT(stgInvTag.getCompoundTag(NBTHelper.SLOT.concat(""+i)));
-		}
-		
-		if(tag.hasKey(NBTHelper.UPGRADESLOT))
-			upgradeSlot = ItemStack.loadItemStackFromNBT(tag.getCompoundTag(NBTHelper.UPGRADESLOT));
-		if(getType().hasArmorSlot() && tag.hasKey(NBTHelper.ARMORSLOT))
-			armorSlot = ItemStack.loadItemStackFromNBT(tag.getCompoundTag(NBTHelper.ARMORSLOT));
-		markDirty();
-	}
-
-	@Override
-	public int getSizeInventory()
-	{
-		return getType().getStorageCount();
+		BackpackInvWrapper.readExtraNBT(tag, inv);
 	}
 	
+	//HELPERS
 	public BackPackType getType()
 	{
 		return BackPackType.values()[Math.max(0, 
 				Math.min(getCurrentLevel(), 
 						BackPackType.values().length-1))];
 	}
-
-	@Override
-	public ItemStack getStackInSlot(int slot)
+	
+	public void setBlockMeta(int meta)
 	{
-		//Helper.getLogger().info((worldObj.isRemote?"Client":"Server")+" getting slot contents at index " + slot);
-		//TODO: change so that the last 3 levels can all mount some form of armor
-		//improved limited to un-enchanted iron or leather armor only; possibly also wood armor from TC
-		//advanced can have any kind of un-enchanted, vanilla armor
-		//aromored has no restrictions, beyond that the item extend ItemArmor
-		boolean isArmor = getType().hasArmorSlot();
-
-		if (slot < storageInventory.length)
-			return storageInventory[slot];
-		if (slot >= storageInventory.length && slot < storageInventory.length + componentInventory.length)
-			return componentInventory[slot - storageInventory.length];
-		if (slot == storageInventory.length + componentInventory.length)
-			return upgradeSlot;
-		if (slot == storageInventory.length + componentInventory.length + 1 && isArmor)
-			return armorSlot;
-		return null;
+		worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, meta, 2);
 	}
-
-	@Override
-	public ItemStack decrStackSize(int slot, int value)
+	
+	public ItemStack getComponentForDropping(UpgradeType componentType)
 	{
-		ItemStack isTemp = getStackInSlot(slot);
-		if(isTemp == null)
-			return null;
-		if(isTemp.stackSize > value)
+		if (componentType == null)
 		{
-			isTemp.stackSize -= value;
-			setInventorySlotContents(slot, isTemp.copy());
-			isTemp.stackSize = value;
-			return isTemp;
+			Helper.getLogger().warn("Unexpected null value in TileEntityBackpack");
+			return null; //this shouldn't happen;
 		}
-		else
-		{
-			setInventorySlotContents(slot, null);
-			return isTemp;
-		}
+		ItemStack itemStack = inv.componentInventory[componentType.ordinal()];
+		inv.componentInventory[componentType.ordinal()] = null;
+		return itemStack;
 	}
-
-	@Override
-	public ItemStack getStackInSlotOnClosing(int slot)
-	{
-		return getStackInSlot(slot);
-	}
-
-	@Override
-	public void setInventorySlotContents(int slot, ItemStack itemStack)
-	{
-		Helper.getLogger().info((worldObj.isRemote?"Client":"Server")+" setting slot contents at index " + slot);
-		boolean isArmor = getType().hasArmorSlot();
-		if(itemStack != null)
-			itemStack = itemStack.copy();
-		
-		if (slot < storageInventory.length)
-			storageInventory[slot] = itemStack;
-		else if (slot >= storageInventory.length && slot < storageInventory.length + componentInventory.length)
-			componentInventory[slot - storageInventory.length] = itemStack;
-		else if (slot == storageInventory.length + componentInventory.length)
-			upgradeSlot = itemStack;
-		else if (slot == storageInventory.length + componentInventory.length + 1 && isArmor)
-			armorSlot = itemStack;
-	}
-
-	@Override
-	public String getInventoryName()
-	{
-		return StatCollector.translateToLocal("inventory.backpack.name");
-	}
-
-	@Override
-	public boolean hasCustomInventoryName()
-	{
-		return false; // TODO: add ability to change the backpack's name in the anvil?
-	}
-
-	@Override
-	public int getInventoryStackLimit()
-	{
-		return 64;
-	}
-
-	@Override
-	public boolean isUseableByPlayer(EntityPlayer var1)
-	{
-		return true;
-	}
-
-	@Override
-	public boolean isItemValidForSlot(int slot, ItemStack itemStack)
-	{
-		Item item = itemStack.getItem();
-		if (slot < storageInventory.length)
-			return true;
-		if (slot >= storageInventory.length && slot < storageInventory.length + componentInventory.length)
-			return Block.getBlockFromItem(item) instanceof BlockCampComponent;
-		if (slot == storageInventory.length + componentInventory.length)
-			return item instanceof ItemUpgrade || Block.getBlockFromItem(item) instanceof BlockCampComponent;
-		if (slot == storageInventory.length + componentInventory.length + 1 && getType().hasArmorSlot())
-			return item instanceof ItemArmor && !(item instanceof ItemBackPack);
-		return false;
-	}
-
-	@Override
-	public void openInventory()
-	{}
-
-	@Override
-	public void closeInventory()
-	{}
-
+	
+	//UPGRADE AND DEPLOYMENT HANDLING
 	/**
 	 * Attempt to apply the upgrade currently in the upgrade slot
 	 * @return true if the upgrade was successfully applied
 	 */
 	public boolean doUpgrade()
 	{
-		if (upgradeSlot == null )
+		if (inv.upgradeSlot == null )
 			return false;
 
-		if(upgradeSlot.getItem() instanceof ItemUpgrade)
+		if(inv.upgradeSlot.getItem() instanceof ItemUpgrade)
 		{
-			ItemUpgrade upgrade = (ItemUpgrade)upgradeSlot.getItem();
-			int dmg = upgradeSlot.getItemDamage(), 
+			ItemUpgrade upgrade = (ItemUpgrade)inv.upgradeSlot.getItem();
+			int dmg = inv.upgradeSlot.getItemDamage(), 
 					lvl = ItemUpgrade.getLevelFromDamage(dmg);
 			UpgradeType type = ItemUpgrade.getTypeFromDamage(dmg);
 			//if the user is upgrading their backpack in-place
@@ -251,48 +113,48 @@ public class TileEntityBackPack extends TileEntityDeployableBase implements IInv
 				if(meta == lvl)
 				{
 					setBlockMeta(meta + 1);
-					Helper.getLogger().info("Applied upgrade "+upgradeSlot.getDisplayName());
-					upgradeSlot = null;
-					ItemStack[] isa = storageInventory;
-					storageInventory = new ItemStack[getType().getStorageCount()];
+					Helper.getLogger().info("Applied upgrade "+inv.upgradeSlot.getDisplayName());
+					inv.upgradeSlot = null;
+					ItemStack[] isa = inv.storageInventory;
+					inv.storageInventory = new ItemStack[getType().getStorageCount()];
 					for(int i = 0; i < isa.length; i++)
-						storageInventory[i] = isa[i];
+						inv.storageInventory[i] = isa[i];
 					
 					return true;
 				}
 				return false;
 			}
 			int typeIndex = type.ordinal(),
-				currentLvl = componentInventory[typeIndex] == null ? -1 : componentInventory[typeIndex].getItemDamage();
+				currentLvl = inv.componentInventory[typeIndex] == null ? -1 : inv.componentInventory[typeIndex].getItemDamage();
 			//if the user is upgrading one of their components and the upgrade levels match
 			if(currentLvl + 1 == lvl)
 			{
-				if(componentInventory[typeIndex] == null)
-					componentInventory[typeIndex] = new ItemStack(type.getBlockContainer());
-				componentInventory[typeIndex].setItemDamage(lvl);
-				if(componentInventory[typeIndex].stackTagCompound == null)
-					componentInventory[typeIndex].stackTagCompound = new NBTTagCompound();
-				upgradeSlot = null;
+				if(inv.componentInventory[typeIndex] == null)
+					inv.componentInventory[typeIndex] = new ItemStack(type.getBlockContainer());
+				inv.componentInventory[typeIndex].setItemDamage(lvl);
+				if(inv.componentInventory[typeIndex].stackTagCompound == null)
+					inv.componentInventory[typeIndex].stackTagCompound = new NBTTagCompound();
+				inv.upgradeSlot = null;
 				return true;
 			}
 			return false;
 		}
-		else if(Block.getBlockFromItem(upgradeSlot.getItem()) instanceof BlockCampComponent)
+		else if(Block.getBlockFromItem(inv.upgradeSlot.getItem()) instanceof BlockCampComponent)
 		{
-			BlockCampComponent block = (BlockCampComponent) Block.getBlockFromItem(upgradeSlot.getItem());
+			BlockCampComponent block = (BlockCampComponent) Block.getBlockFromItem(inv.upgradeSlot.getItem());
 			int index = block.getType().ordinal();
-			if(upgradeSlot == null)
+			if(inv.upgradeSlot == null)
 			{
-				componentInventory[index] = upgradeSlot;
+				inv.componentInventory[index] = inv.upgradeSlot;
 				Helper.getLogger().info("Applied existing component to empty slot");
-				upgradeSlot = null;
+				inv.upgradeSlot = null;
 				return true;
 			}
 			else
 			{
-				ItemStack temp = componentInventory[index];
-				componentInventory[index] = upgradeSlot;
-				upgradeSlot = temp;
+				ItemStack temp = inv.componentInventory[index];
+				inv.componentInventory[index] = inv.upgradeSlot;
+				inv.upgradeSlot = temp;
 				Helper.getLogger().info("Swapped upgrade component with existing item in slot");
 				return true;
 			}
@@ -302,24 +164,14 @@ public class TileEntityBackPack extends TileEntityDeployableBase implements IInv
 		return false;
 	}
 
-	public ItemStack getComponentForDropping(UpgradeType componentType)
-	{
-		if (componentType == null)
-		{
-			Helper.getLogger().warn("Unexpected null value in TileEntityBackpack");
-			return null; //this shouldn't happen;
-		}
-		ItemStack itemStack = componentInventory[componentType.ordinal()];
-		componentInventory[componentType.ordinal()] = null;
-		return itemStack;
-	}
+
 	public void deployAll()
 	{
 		NBTTagCompound tag;
-		for (int i = 0; i < componentInventory.length; i++)
-			if(componentInventory[i] != null)
+		for (int i = 0; i < inv.componentInventory.length; i++)
+			if(inv.componentInventory[i] != null)
 			{
-				tag = componentInventory[i].stackTagCompound;
+				tag = inv.componentInventory[i].stackTagCompound;
 				if(!tag.getBoolean(NBTHelper.IS_DEPLOYED))
 					toggle(i);
 			}
@@ -327,17 +179,17 @@ public class TileEntityBackPack extends TileEntityDeployableBase implements IInv
 	public void recoverAll()
 	{
 		NBTTagCompound tag;
-		for (int i = 0; i < componentInventory.length; i++)
-			if(componentInventory[i] != null)
+		for (int i = 0; i < inv.componentInventory.length; i++)
+			if(inv.componentInventory[i] != null)
 			{
-				tag = componentInventory[i].stackTagCompound;
+				tag = inv.componentInventory[i].stackTagCompound;
 				if(tag.getBoolean(NBTHelper.IS_DEPLOYED))
 					toggle(i);
 			}
 	}
 	public void toggle(int slot)
 	{
-		ItemStack upgrade = componentInventory[slot];
+		ItemStack upgrade = inv.componentInventory[slot];
 		if (upgrade == null)
 			return;
 		NBTTagCompound comTag = upgrade.getTagCompound();
@@ -352,8 +204,5 @@ public class TileEntityBackPack extends TileEntityDeployableBase implements IInv
 			worldObj.getTileEntity(xCoord + xoffset, yCoord + yoffset, zCoord + zoffset).readFromNBT(comTag);
 	}
 
-	public void setBlockMeta(int meta)
-	{
-		worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, meta, 2);
-	}
+
 }
