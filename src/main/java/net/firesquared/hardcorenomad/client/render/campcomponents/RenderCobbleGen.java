86fbf05531aab4c622a610e697a6c42e8e1421d1
FusionLord <FusionLord@live.com>
2014-05-27 17:24:41 -0700

    More work on cobble generator model animation;

--- src/main/java/net/firesquared/hardcorenomad/client/render/campcomponents/RenderCobbleGen.java
+++ src/main/java/net/firesquared/hardcorenomad/client/render/campcomponents/RenderCobbleGen.java
@@ -9,50 +9,56 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.model.IModelCustom;
 import net.minecraftforge.client.model.obj.WavefrontObject;
 import org.lwjgl.opengl.GL11;
 
 public class RenderCobbleGen extends RenderCampComp
 {
+	IModelCustom vat = ModelRegistry.getModel(Models.COBBLEGEN_VAT);
+
 	@Override
 	protected void renderTile(TileEntityDeployableBase tile, int lighting)
 	{
-		TileEntityCobbleGenerator cobbleGen = (TileEntityCobbleGenerator)tile; 
-		int time = cobbleGen.getCurrentProcessTime() + 1;
+		TileEntityCobbleGenerator cobbleGen = (TileEntityCobbleGenerator)tile;
+		float percentage = cobbleGen.getPercentage();
+		float rot = cobbleGen.getRotation();
+
 		GL11.glTranslatef(.5f, 0f, .5f);
 		model = ModelRegistry.getModel(Models.COBBLEGEN);
 		texture = ModelRegistry.getTexture(Models.COBBLEGEN);
 		bindTexture(texture);
 		model.renderOnly("trough", "outputvat");
-		
-		float height = time / 1000f;
-		float rotation = (time < 900 ? 0 : 45f * (time / 50f));
+		bindTexture(TextureMap.locationBlocksTexture);
+		RendererUtil.renderPartWithIcon((WavefrontObject) model, "output", Blocks.cobblestone.getIcon(0, 0), Tessellator.instance, -1);
 		GL11.glPushMatrix();
 		{
-			GL11.glTranslatef(0f, height, 0f);
-			model.renderPart("arm");
+			if (percentage > .75f)
+				percentage = .75f;
+			GL11.glTranslatef(0f, percentage, 0f);
+			bindTexture(texture);
+			model.renderOnly("armlava", "armwater");
+			GL11.glTranslatef(-.35f, .15f, -0.3375f);
 			GL11.glPushMatrix();
 			{
-				GL11.glRotatef(-rotation, 0f, 0f, 1f);
-				model.renderPart("lavavat");
+				GL11.glRotatef(-rot, 0f, 0f, 1f);
+				vat.renderPart("vat");
 				bindTexture(TextureMap.locationBlocksTexture);
-				RendererUtil.renderPartWithIcon((WavefrontObject) model, "lava", Blocks.lava.getIcon(0, 0),
-						Tessellator.instance, -1);
-				GL11.glRotatef(rotation * 2, 0f, 0f, 1f);
+				RendererUtil.renderPartWithIcon((WavefrontObject)vat, "contents", Blocks.lava.getIcon(0, 0), Tessellator.instance, -1);
+			}
+			GL11.glPopMatrix();
+			GL11.glTranslatef(.7f, 0f, 0f);
+			GL11.glPushMatrix();
+			{
+				GL11.glRotatef(rot, 0f, 0f, 1f);
 				bindTexture(texture);
-				model.renderPart("watervat");
+				vat.renderPart("vat");
 				bindTexture(TextureMap.locationBlocksTexture);
-				RendererUtil.renderPartWithIcon((WavefrontObject) model, "water", Blocks.water.getIcon(0, 0),
-						Tessellator.instance, -1);
+				RendererUtil.renderPartWithIcon((WavefrontObject)vat, "contents", Blocks.water.getIcon(0, 0), Tessellator.instance, -1);
 			}
 			GL11.glPopMatrix();
 		}
 		GL11.glPopMatrix();
-
-		RendererUtil.renderPartWithIcon((WavefrontObject) model, "output", Blocks.cobblestone.getIcon(0, 0),
-				Tessellator.instance, -1);
-		if (/* (TileEntityCobbleGen)tile.time > 100 && (TileEntityCobbleGen)tile.time < 130 */ true)
-			RendererUtil.renderPartWithIcon((WavefrontObject)model, "cobble", Blocks.cobblestone.getIcon(0, 0), Tessellator.instance, -1);
 	}
 
 	@Override
--- src/main/java/net/firesquared/hardcorenomad/tile/campcomponents/TileEntityCobbleGenerator.java
+++ src/main/java/net/firesquared/hardcorenomad/tile/campcomponents/TileEntityCobbleGenerator.java
@@ -6,6 +6,9 @@ import net.firesquared.hardcorenomad.tile.TileEntityDeployableBase;
 public class TileEntityCobbleGenerator extends TileEntityDeployableBase
 {
 	private int currentProcessTime;
+	private int currentMaxTime = 1000;
+	private float percentage;
+	private float rotation;
 
 	public TileEntityCobbleGenerator()
 	{
@@ -17,13 +20,30 @@ public class TileEntityCobbleGenerator extends TileEntityDeployableBase
 		return currentProcessTime;
 	}
 
+	public int getCurrentMaxTime()
+	{
+		return currentMaxTime;
+	}
+
+	public float getPercentage()
+	{
+		return percentage;
+	}
+
+	public float getRotation()
+	{
+		return rotation;
+	}
+
 	@Override
 	public void updateEntity()
 	{
 		super.updateEntity();
-		if (currentProcessTime++ > 1000)
+		if (currentProcessTime++ > currentMaxTime)
 		{
-			currentProcessTime = 0;
+			currentProcessTime = 1;
 		}
+		percentage = (float)currentProcessTime / (float) currentMaxTime;
+		rotation = percentage > .75f ? (1f - percentage) : 0f;
 	}
 }
