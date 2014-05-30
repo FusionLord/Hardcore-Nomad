713781db78dff0ccdcd2ab64f4a31ecbc116d3dd
FireBall1725 <treed@sunnking.com>
2014-05-30 18:16:14 -0400

    Removed Loot Chest from Items

--- src/main/java/net/firesquared/hardcorenomad/helpers/enums/Items.java
+++ src/main/java/net/firesquared/hardcorenomad/helpers/enums/Items.java
@@ -16,16 +16,18 @@ import net.minecraft.util.StatCollector;
 public enum Items
 {
 	// TODO: Add Items
-	// Example ITEM_NAME("item.name", new ItemClass())
 	ITEM_BACKPACK("item.backpack", new ItemBackPack(), CreativeTab.HardCoreNomadTab),
+
 	//ITEM_FIREBOW("item.firebow", new ItemFireBow(0.0F, null, null)),
 	//ITEM_FIREBUNDLE("campfire.bundle", new ItemFireBundle(), CreativeTab.HardCoreNomadTab),
 	//ITEM_HEALINGFIRSTAID("healing.firstaid", new ItemHealingFirstAid(), CreativeTab.HardCoreNomadTab),
-	ITEM_HEALINGHERB("healing.herb", new ItemHealingHerb(2, 1.0F, false), CreativeTab.HardCoreNomadTab, 1, 5, 4),
+
+	ITEM_HEALINGHERB("healing.herb", new ItemHealingHerb(2, 1.0F, false), CreativeTab.HardCoreNomadTab),
 	//ITEM_HEALINGMAGICALAID("healing.magicalaid", new ItemHealingMagicalAid(), CreativeTab.HardCoreNomadTab),
+
 	ITEM_FLINTSHEARS("tools.flintshears", new ItemShears().setMaxDamage(42).setTextureName("hardcorenomad:flintshears"), CreativeTabs.tabTools),
 
-	ITEM_MISC_PEBBLE("misc.pebble", new ItemPebble(), CreativeTab.HardCoreNomadTab, 1, 23, 5),
+	ITEM_MISC_PEBBLE("misc.pebble", new ItemPebble(), CreativeTab.HardCoreNomadTab),
 	ITEM_MISC_SLINGSHOT("misc.slingshot", new ItemSlingShot(), CreativeTab.HardCoreNomadTab),
 
 	//Upgrade Items
@@ -34,33 +36,15 @@ public enum Items
 	;
 
 	private final String internalName;
-	public final boolean isDungeonLoot;
 	public final Item item;
-	private int dungeonChestMin;
-	private int dungeonChestMax;
-	private int weight;
 
 	Items(String internalName, Item item, CreativeTabs creativeTabs)
-	{
-		this(internalName, item, creativeTabs, -1, -1, -1);
-	}
-
-	Items(String internalName, Item item, CreativeTabs creativeTabs, int dungeonChestMin, int dungeonChestMax, int weight) 
 	{
 		this.internalName = internalName;
 		this.item = item;
 		item.setUnlocalizedName(Helper.Strings.MOD_ID + "." + internalName);
 		item.setTextureName(Helper.Strings.MOD_ID + ":textures.item."+internalName);
 		item.setCreativeTab(creativeTabs);
-		if(dungeonChestMax < 0 || dungeonChestMin < 0 || weight < 0)
-			isDungeonLoot = false;
-		else
-		{
-			this.dungeonChestMax = dungeonChestMax;
-			this.dungeonChestMin = dungeonChestMin;
-			this.weight = weight;
-			isDungeonLoot = true;
-		}
 	}
 	
 	private void register()
@@ -84,18 +68,6 @@ public enum Items
 	public ItemStack getStack(int damage, int size)
 	{
 		return new ItemStack(item, size, damage);
-	}
-
-	public int getDungeonChestMin() {
-		return isDungeonLoot ? this.dungeonChestMin : -1;
-	}
-
-	public int getDungeonChestMax() {
-		return isDungeonLoot ? this.dungeonChestMax : -1;
-	}
-
-	public int getWeight() {
-		return isDungeonLoot ? this.weight : -1;
 	}
 
 	public static void registerAll()
